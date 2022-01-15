a2ad3b9e9261a47c1765cfc1216023da2c278e77
Alok Raj <alokr417@gmail.com>
2022-01-15 21:55:50 +0530

    ✔ DB schema included

---
+++ DB/crime_portal.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2018 at 04:19 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crime_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 'Null'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`c_id`, `a_no`, `location`, `type_crime`, `d_o_c`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(1, 123214521452, 'Tollygunge', 'Robbery', '2018-12-06', 'My Home has been Robbed.', 'Assigned', 'ChargeSheet Filed', 't101');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `head`
+--
+
+CREATE TABLE `head` (
+  `h_id` varchar(50) NOT NULL,
+  `h_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `head`
+--
+
+INSERT INTO `head` (`h_id`, `h_pass`) VALUES
+('head@kp', 'head');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `p_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police`
+--
+
+INSERT INTO `police` (`p_name`, `p_id`, `spec`, `location`, `p_pass`) VALUES
+('Manish Singh', 'a101', 'Murder', 'Anandapur', 'manish'),
+('Jay Singh', 'a102', 'All', 'Anandapur', 'jay'),
+('Suvendu Ghosh', 't101', 'Robbery', 'Tollygunge', 'suvendu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police_station`
+--
+
+CREATE TABLE `police_station` (
+  `i_id` varchar(50) NOT NULL,
+  `i_name` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `i_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police_station`
+--
+
+INSERT INTO `police_station` (`i_id`, `i_name`, `location`, `i_pass`) VALUES
+('shah@anandapur', 'Shahbaz', 'Anandapur', 'shahbaz'),
+('shivam@tollygunge', 'Shivam', 'Tollygunge', 'shivam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_case`
+--
+
+CREATE TABLE `update_case` (
+  `c_id` int(11) NOT NULL,
+  `d_o_u` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `case_update` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `update_case`
+--
+
+INSERT INTO `update_case` (`c_id`, `d_o_u`, `case_update`) VALUES
+(1, '2018-12-17 10:32:06', 'Criminal Verified'),
+(1, '2018-12-17 10:32:12', 'Criminal Caught'),
+(1, '2018-12-17 10:32:15', 'Criminal Interrogated'),
+(1, '2018-12-17 10:32:21', 'Criminal Accepted the Crime'),
+(1, '2018-12-17 10:32:26', 'Criminal Charged'),
+(1, '2018-12-17 10:32:51', 'The case has been moved to Court.'),
+(1, '2018-12-17 10:32:59', 'Criminal Verified');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('Satyansh Kumar', 'satyansh123@gmail.com', 'satyansh', 'Ranchi', 123214521452, 'Male', 9854123654);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `police_station`
+--
+ALTER TABLE `police_station`
+  ADD PRIMARY KEY (`i_id`),
+  ADD UNIQUE KEY `location` (`location`);
+
+--
+-- Indexes for table `update_case`
+--
+ALTER TABLE `update_case`
+  ADD UNIQUE KEY `d_o_u` (`d_o_u`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -12,46 +12,41 @@
   </a>
 </p>
 
-PHP Training Project: It is a website where user can log an FIR online. This website has 4 module- Head-quarter, Incharge, Police, User. It is multifunctional website with back end support
-Project Objective:
-1. •	To provide an online platform where user can log a FIR at ease.
-2. •	Complainant can see the regular update of the filed case.
-3. •	Head Quarter can easily monitor the update of individual case.
-4. •	To provide transparency in working system of the case.
-5. •	It will reduce the bribe.
+> A website where user can log an FIR online. This website has 4 modules: Head-quarter, Police In-charge, Police, User. It is multifunctional website with back end support.
 
-Project Scope :  
-Complainant Module:-
+## Project Objective
+1. To provide an online platform where user can log a FIR at ease.
+2. Complainant can see the regular update of the filed case.
+3. Head Quarter can easily monitor the update of individual case.
+4. To provide transparency in working system of the case.
+5. It will reduce the bribe.
 
-	Complainant can file an FIR online and can see the regular update of the filed case. 
-	He/She can file multiple FIR .
+## Project Scope
+### Complainant Module
 
-HQ Module:- 
+-	Complainant can file an FIR online and can see the regular update of the filed case. 
+-	He/She can file multiple FIR .
 
-	HQ can monitor every case.
- 
-	HQ can add or delete the police station.
+### HQ Module
 
-	HQ will provide login credential to Incharge. 
+-	HQ can monitor every case.
+-	HQ can add or delete the police station.
+-	HQ will provide login credential to Police In-charge. 
 
-Incharge Module:-
+### In-charge Module
+-	In-charge can see the case allocated to his location.
+-	He/She can add police officers for his/her location. 
+-	In-charge will assign the case to each police.
+-	In-charge will provide login credential to Police.
 
+### Police Module
+-	Police can only see the details of that case which has been assigned by the In-charge.
+-	Police can update the status of each case.
+-	Police can close the case and file the charge-sheet.
 
-	Incharge can see the case allocated to his location.
+## [Database Schema](./DB)
 
-	He/She can add police officers for his/her location. 
-
-	Incharge will assign the case to each police.
-
-	Incharge will provide login credential to Police.
-
-Police Module:-
-
-	Police can only see the details of that case which has been assigned by the Incharge.
-	Police can update the status of each case.
-	Police can close the case and file the chargesheet.
-
-### ✨ Demo (Few) :
+### ✨ Screenshots (Few) :
   ![](screenshot/home.jpg)
   ![](screenshot/complain.jpg)
   ![](screenshot/history.jpg)
@@ -72,13 +67,13 @@ Supported on Xampp v5.6.36
 * Github: [@alok722](https://github.com/alok722)
 * LinkedIn: [@alok722](https://linkedin.com/in/alok722)
 
-## 🗒 Upcoming Plans
+## 🗒 Future Plans
 
-1. OTP verification can be added to authenticate the user.
-2. Data Sharing between police station can be incorporated.
-3. Wanted Criminal reward system can be added.
-4. Chat Support Between Officers can be added.
-5. Location of crime will be automatically detected just by pointing on the map.
+- OTP verification can be added to authenticate the user.
+- Data Sharing between police station can be incorporated.
+- Wanted Criminal reward system can be added.
+- Chat Support Between Officers can be added.
+- Location of crime will be automatically detected just by pointing on the map.
 
 ## 🤝 Contributing
 
